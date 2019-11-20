ced07e161fe964de26106c4f603134e9c6cce56b
danushka96 <danushkaherath96@gmail.com>
2019-11-20 22:15:05 +0530

    changed ui

--- src/components/TaskContent.vue
+++ src/components/TaskContent.vue
@@ -1,119 +1,90 @@
 <template>
-  <div>
+    <div>
+        <v-list-item>
+            <v-speed-dial right top v-model="fab" direction="right">
+                <template v-slot:activator>
+                    <v-btn color="blue darken-2" dark fab style="margin-left: 10px; margin-bottom: 35px !important;" v-model="fab" x-small  class="my-2" >
+                        <v-icon v-if="fab">mdi-close</v-icon>
+                        <v-icon v-else>mdi-pencil</v-icon>
+                    </v-btn>
+                </template>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                  <v-btn color="green" dark fab small v-on="on" @click="updateTask(0)">
+                      <v-icon>mdi-check</v-icon>
+                  </v-btn>
+                </template>
+                <span>Completed</span>
+              </v-tooltip>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                  <v-btn color="indigo" dark fab small v-on="on" @click="updateTask(1)">
+                      <v-icon>mdi-progress-check</v-icon>
+                  </v-btn>
+                </template>
+                <span>In Progress</span>
+              </v-tooltip>
+              <v-tooltip bottom>
+                <template v-slot:activator="{ on }">
+                  <v-btn color="orange" dark fab small v-on="on" @click="updateTask(2)">
+                      <v-icon>mdi-progress-alert</v-icon>
+                  </v-btn>
+                </template>
+                <span>Assigned</span>
+              </v-tooltip>
+            </v-speed-dial>
+            <v-list-item-content>
+                {{content.description}}
+            </v-list-item-content>
+            <v-btn @click="deleteConfirm=true" color="red" dark fab small text>
+                <v-icon dark>mdi-delete</v-icon>
+            </v-btn>
+        </v-list-item>
+      <v-dialog v-model="deleteConfirm" max-width="290">
+        <v-card>
+          <v-card-title class="headline">Are you sure?</v-card-title>
 
-<!--    <v-card class="mx-auto" max-width="97%" :hover="true" :dark="true" :loading="false" outlined>-->
-<!--      <v-card-title>{{content.title}}</v-card-title>-->
-<!--      <v-card-text>-->
-<!--          <p>{{content.description}}</p>-->
-<!--          <v-chip v-for="(value,key) of content.type" :key="key">{{value}}</v-chip>-->
-<!--          </v-card-text>-->
-<!--      <v-card-actions>-->
-<!--        <v-btn color="blue" @click="editDialog=true" text>Edit</v-btn>-->
-<!--        <v-btn color="red" @click="deleteConfirm=true" text>Delete</v-btn>-->
-<!--      </v-card-actions>-->
-<!--    </v-card>-->
-    <v-dialog v-model="editDialog" persistent max-width="600px">
-      <v-card>
-        <v-card-title>
-          <span class="headline">Edit Task</span>
-        </v-card-title>
-        <v-card-text>
-          <v-form ref="form" v-model="valid">
-            <v-container>
-              <v-row>
-                <v-col cols="12" sm="6" md="6">
-                  <v-text-field label="Title*" :rules="titleRules" v-model="title" required></v-text-field>
-                </v-col>
-                <v-col cols="12" sm="6" md="6">
-                  <v-autocomplete
-                    :items="['Completed', 'Todo', 'Failed']"
-                    v-model="type"
-                    :rules="typeRules"
-                    label="Type"
-                    multiple
-                  ></v-autocomplete>
-                </v-col>
-                <v-col cols="12">
-                  <v-textarea
-                    label="Description*"
-                    v-model="description"
-                    :rules="descriptionRules"
-                    required
-                  ></v-textarea>
-                </v-col>
-              </v-row>
-            </v-container>
-            <small>*indicates required field</small>
-          </v-form>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text @click="editDialog = false">Close</v-btn>
-          <v-btn color="blue darken-1" text @click="editTask()" :disabled="!valid">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <v-dialog v-model="deleteConfirm" max-width="290">
-      <v-card>
-        <v-card-title class="headline">Are you sure?</v-card-title>
+          <v-card-text>I sure hope you know what you are doing..</v-card-text>
 
-        <v-card-text>I sure hope you know what you are doing..</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
 
-        <v-card-actions>
-          <v-spacer></v-spacer>
+            <v-btn color="green darken-1" text @click="deleteConfirm = false">Cancel</v-btn>
 
-          <v-btn color="green darken-1" text @click="deleteConfirm = false">Cancel</v-btn>
-
-          <v-btn color="red darken-1" text @click="deleteTask()">Delete</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </div>
+            <v-btn color="red darken-1" text @click="deleteTask()">Delete</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
 </template>
 
 <script>
-export default {
-  props: {
-    content: Object
-  },
-  beforeMount() {
-    this.title = this.content.title;
-    this.type = this.content.type;
-    this.description = this.content.description;
-  },
-  data: () => ({
-    valid: false,
-    deleteConfirm: false,
-    editDialog: false,
-    title: "",
-    titleRules: [
-      v => !!v || "Title is required",
-      v => (v && v.length > 3) || "Title must be greater than 3 characters"
-    ],
-    type: "",
-    typeRules: [v => !!v || "Type is required"],
-    description: "",
-    descriptionRules: [
-      v => !!v || "Description is required",
-      v =>
-        (v && v.length > 3) || "Description must be greater than 5 characters"
-    ]
-  }),
-  methods: {
-    editTask() {
-      this.$emit("edit", {
-        ".key": this.content[".key"],
-        title: this.title,
-        type: this.type,
-        description: this.description
-      });
-      this.editDialog = false;
-    },
-    deleteTask(){
-        this.$emit("delete", {
-            ".key": this.content[".key"]
-        })
-    }
-  }
-};
+    export default {
+        props: {
+            content: Object
+        },
+        beforeMount() {
+            this.title = this.content.title;
+            this.type = this.content.type;
+            this.description = this.content.description;
+        },
+        data: () => ({
+            fab: '',
+            deleteConfirm: false
+        }),
+        methods: {
+            deleteTask() {
+                this.$emit("delete", {
+                    ".key": this.content[".key"]
+                });
+              this.deleteConfirm = false
+            },
+            updateTask(state){
+                this.$emit("update", {
+                    ".key": this.content[".key"],
+                    "state": state
+                })
+            }
+        }
+    };
 </script>
--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -2,76 +2,134 @@
   <div>
     <v-row>
       <v-col cols="12" sm="12">
-        <v-card class="mx-auto" tile dark>
-          <v-list rounded>
-            <v-subheader>TASKS</v-subheader>
-            <v-list-item-group v-model="item" color="primary">
-              <v-list-item v-for="(value,key) of getSelectedDateTasks" :key="key">
-                <v-list-item-icon>
-                  <v-icon>mdi-flag</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                  {{value.description}}
-                </v-list-item-content>
-                <v-btn color="red" @click="deleteConf(value)" text fab dark small>
-                  <v-icon dark>mdi-delete</v-icon>
-                </v-btn>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>mdi-flag</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
-                <v-textarea
-                        name="input-1-1"
-                        label="Task Description"
-                        hint="Press Enter to save the Task"
-                        autofocus
-                        rows="3"
-                        v-model="description"
-                        @keyup.enter.native="saveTask()"
-                ></v-textarea>
-                </v-list-item-content>
-              </v-list-item>
-            </v-list-item-group>
-          </v-list>
+        <v-card>
+          <v-tabs background-color="deep-purple accent-4" center-active dark v-model="tab">
+            <v-tab href="#tab-1">All Tasks</v-tab>
+            <v-tab href="#tab-2" @click="selected=0">Completed</v-tab>
+            <v-tab href="#tab-3" @click="selected=1">Progress</v-tab>
+            <v-tab href="#tab-4" @click="selected=2">Assigned</v-tab>
+          </v-tabs>
         </v-card>
+        <v-tabs-items v-model="tab">
+          <v-tab-item value="tab-1">
+            <v-card class="mx-auto" tile dark>
+              <v-list rounded>
+                <v-subheader>TASKS</v-subheader>
+                <v-list-item-group v-model="item" color="primary">
+                  <task-content @delete="deleteTask" v-for="(value,key) of getSelectedDateTasks"
+                                @update="editTask" :key="key" :content="value"></task-content>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-textarea
+                              name="input-1-1"
+                              label="Task Description"
+                              hint="Press Enter to save the Task"
+                              autofocus
+                              rows="3"
+                              v-model="description"
+                              @keyup.enter.native="saveTask()"
+                      ></v-textarea>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item value="tab-2">
+            <v-card class="mx-auto" tile dark>
+              <v-list rounded>
+                <v-subheader>TASKS</v-subheader>
+                <v-list-item-group v-model="item" color="primary">
+                  <task-content @delete="deleteTask" v-for="(value,key) of getTaskList"
+                                @update="editTask" :key="key" :content="value"></task-content>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-textarea
+                              name="input-1-1"
+                              label="Task Description"
+                              hint="Press Enter to save the Task"
+                              autofocus
+                              rows="3"
+                              v-model="description"
+                              @keyup.enter.native="saveTask()"
+                      ></v-textarea>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item value="tab-3">
+            <v-card class="mx-auto" tile dark>
+              <v-list rounded>
+                <v-subheader>TASKS</v-subheader>
+                <v-list-item-group v-model="item" color="primary">
+                  <task-content @delete="deleteTask" v-for="(value,key) of getTaskList"
+                                @update="editTask" :key="key" :content="value"></task-content>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-textarea
+                              name="input-1-1"
+                              label="Task Description"
+                              hint="Press Enter to save the Task"
+                              autofocus
+                              rows="3"
+                              v-model="description"
+                              @keyup.enter.native="saveTask()"
+                      ></v-textarea>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+          </v-tab-item>
+          <v-tab-item value="tab-4">
+            <v-card class="mx-auto" tile dark>
+              <v-list rounded>
+                <v-subheader>TASKS</v-subheader>
+                <v-list-item-group v-model="item" color="primary">
+                  <task-content @delete="deleteTask" v-for="(value,key) of getTaskList"
+                                @update="editTask" :key="key" :content="value"></task-content>
+                  <v-list-item>
+                    <v-list-item-content>
+                      <v-textarea
+                              name="input-1-1"
+                              label="Task Description"
+                              hint="Press Enter to save the Task"
+                              autofocus
+                              rows="3"
+                              v-model="description"
+                              @keyup.enter.native="saveTask()"
+                      ></v-textarea>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+
       </v-col>
       <v-col cols="12" sm="6" v-if="getSelectedDateTasks.length === 0">
          <p> No any tasks</p>
       </v-col>
     </v-row>
-    <v-dialog v-model="dialog" max-width="290">
-      <v-card>
-        <v-card-title class="headline">Are you sure?</v-card-title>
-
-        <v-card-text>I sure hope you know what you are doing..</v-card-text>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-
-          <v-btn color="green darken-1" text @click="dialog = false">Cancel</v-btn>
-
-          <v-btn color="red darken-1" text @click="deleteTask()">Delete</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
   </div>
 </template>
 
 <script>
-// import TaskContent from "@/components/TaskContent.vue";
+import TaskContent from "@/components/TaskContent.vue";
 import { db } from "../config/firebase";
 import firebase from "firebase";
 
 export default {
   components: {
-    // TaskContent
+    TaskContent
   },
   data: () => ({
+    tab: 'tab-1',
     taskList: [],
-    selected: '',
-    dialog: false,
+    selected: 0,
     description: "",
     descriptionRules: [
       v => !!v || "Description is required",
@@ -93,14 +151,14 @@ export default {
       });
       this.description = ""
     },
-    deleteTask() {
-      this.$firestore.taskList.doc(this.selected[".key"]).delete();
-      this.dialog = false;
+    deleteTask(task) {
+      this.$firestore.taskList.doc(task[".key"]).delete();
     },
-    deleteConf(value){
-      this.dialog = true;
-      this.selected = value;
-    },
+    editTask(task) {
+      this.$firestore.taskList.doc(task[".key"]).update({
+        state: task.state
+      });
+    }
   },
   computed: {
     currentDate() {
@@ -108,6 +166,9 @@ export default {
     },
     getSelectedDateTasks() {
       return this.taskList.filter(task => task.date === this.currentDate);
+    },
+    getTaskList(){
+      return this.taskList.filter(task => task.date === this.currentDate).filter(item => item.state === this.selected);
     }
   }
 };
